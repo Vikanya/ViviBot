c8828707b57027efa9a64abfd37fe1241f6bc6ad
Vikanya <flavien.birette@orange.fr>
2020-12-19 23:28:42 +0100

    azd

--- commands/yena.js
+++ commands/yena.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'yena',
 	description: 'yena yena yena yena yena yena yena yena ',
 	execute(message, args) {
-		mesage.channel.send('https://gfycat.com/optimalconsciousambushbug');
+		message.channel.send('https://gfycat.com/optimalconsciousambushbug');
 	},
 };
