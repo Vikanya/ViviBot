f80f9ee5a3b41c60cd336752ff8df32717f3890d
Vikanya <flavien.birette@orange.fr>
2020-12-19 18:20:26 +0100

    avatar command

--- ViviBot.js
+++ ViviBot.js
@@ -31,6 +31,19 @@ client.on('message', message => {
 			message.reply('pong');
 		}
 	}
+	else if (command === 'avatar') {
+	if (!message.mentions.users.size) {
+		return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+	}
+
+	const avatarList = message.mentions.users.map(user => {
+		return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+	});
+
+	// send the entire array of strings as a message
+	// by default, discord.js will `.join()` the array with `\n`
+	message.channel.send(avatarList);
+}
 });
 
 client.login(process.env.BOT_TOKEN);
