00bb333155df45984b48cf694542af164cbd5622
Vikanya <flavien.birette@orange.fr>
2020-12-19 20:26:42 +0100

    sdefv

--- index.js
+++ index.js
@@ -11,40 +11,42 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.author.bot) return;
 
-	if (message.mentions.users.size) {
-		/* message.channel.send(message.mentions.users.first() + ' and ' + client.user
-			+ ' test ' + (message.mentions.users.first().equals(client.user)));*/
-		if (message.mentions.users.first().equals(client.user)) {
-			return message.channel.send(':robot: *Fired up and ready to serve.*');
+	
+
+	if (message.content.startsWith(prefix)) {
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		const command = args.shift().toLowerCase();
+		if (command === 'ping') {
+			if (args.length) {
+				message.reply('pong ' + args);
+			}
+			else {
+				message.reply('pong');
+			}
+		}
+		else if (command === 'avatar') {
+			if (!message.mentions.users.size) {
+				return message.channel.send('Your avatar: ' + message.author.displayAvatarURL({ format: "png", dynamic: true }));
+			}
+			const avatarList = message.mentions.users.map(user => {
+				return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+			});
+
+			// send the entire array of strings as a message
+			// by default, discord.js will `.join()` the array with `\n`
+			message.channel.send(avatarList);
+		}		
+	}
+	else {
+		if (message.mentions.users.size) {
+			/* message.channel.send(message.mentions.users.first() + ' and ' + client.user
+				+ ' test ' + (message.mentions.users.first().equals(client.user)));*/
+			if (message.mentions.users.first().equals(client.user)) {
+				return message.channel.send(':robot: *Fired up and ready to serve.*');
+			}
 		}
 	}
 
-	if (!message.content.startsWith(prefix)) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-	if (command === 'ping') {
-		if (args.length) {
-			message.reply('pong ' + args);
-		}
-		else {
-			message.reply('pong');
-		}
-	}
-	else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			message.channel.send('Your avatar ' + message.author.displayAvatarURL({ format: 'png', dynamic: true }));
-			// return message.channel.send('Your avatar: ' + message.author.displayAvatarURL({ format: "png", dynamic: true }));
-		}
-		/*
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
-		});
-
-		// send the entire array of strings as a message
-		// by default, discord.js will `.join()` the array with `\n`
-		message.channel.send(avatarList);*/
-	}
 });
 
 client.login(process.env.BOT_TOKEN);
