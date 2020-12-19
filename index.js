5b325721ff8c05765494df2f5415aa5ff9120525
Vikanya <flavien.birette@orange.fr>
2020-12-19 20:48:37 +0100

    zef

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('message', message => {
 				return message.channel.send('Your avatar: ' + message.author.displayAvatarURL({ format: "png", dynamic: true }));
 			}
 			const avatarList = message.mentions.users.map(user => {
-				return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+				return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
 			});
 
 			// send the entire array of strings as a message
