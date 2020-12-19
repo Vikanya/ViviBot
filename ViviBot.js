d9b2dae50955fb6de225bfe78f988335727943a5
Vikanya <flavien.birette@orange.fr>
2020-12-19 18:21:59 +0100

    fix

--- ViviBot.js
+++ ViviBot.js
@@ -32,18 +32,18 @@ client.on('message', message => {
 		}
 	}
 	else if (command === 'avatar') {
-	if (!message.mentions.users.size) {
-		return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+		});
+
+		// send the entire array of strings as a message
+		// by default, discord.js will `.join()` the array with `\n`
+		message.channel.send(avatarList);
 	}
-
-	const avatarList = message.mentions.users.map(user => {
-		return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
-	});
-
-	// send the entire array of strings as a message
-	// by default, discord.js will `.join()` the array with `\n`
-	message.channel.send(avatarList);
-}
 });
 
 client.login(process.env.BOT_TOKEN);
