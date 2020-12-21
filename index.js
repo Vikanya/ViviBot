73e521e60efad2b0665851bf550b90d531265376
Vikanya <flavien.birette@orange.fr>
2020-12-21 04:01:50 +0100

    efz

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ client.on('message', message => {
 			let resultMessage = message.cleanContent.replace(client.user.username, '').replace('@', '').toLowerCase().trim();
 			
 			if (resultMessage.includes('merci')) {
-				return message.reply('de rien <:viviDisgust:696754743409770588>');
+				return message.reply('de rien <:Finger_Guns:774245239178133515>');
 			}
 			return message.channel.send(':robot: *Fired up and ready to serve.*');
 		}
