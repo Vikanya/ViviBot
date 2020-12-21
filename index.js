30c86552e3ee58e2bfcbe48a6e17ae59e8927438
Vikanya <flavien.birette@orange.fr>
2020-12-21 03:52:12 +0100

    azdazd
    az

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ client.on('message', message => {
 			let resultMessage = message.cleanContent.replace(client.user.username, '').replace('@', '').toLowerCase().trim();
 			
 			if (resultMessage.includes('merci')) {
-				return message.reply('de rien :viviDisgust:');
+				return message.reply('de rien ' + client.emojis.find(emoji => emoji.name === "viviDisgust"));
 			}
 			return message.channel.send(':robot: *Fired up and ready to serve.*');
 		}
