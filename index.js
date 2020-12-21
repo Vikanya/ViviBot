1436202e0a0ad06e8229403f3f02aa9cbf69e813
Vikanya <flavien.birette@orange.fr>
2020-12-21 03:14:27 +0100

    raq

--- index.js
+++ index.js
@@ -132,8 +132,8 @@ client.on('message', message => {
 			+ ' test ' + (message.mentions.users.first().equals(client.user)));*/
 		if (message.mentions.users.first().equals(client.user)) {
 			const resultMessage = message.cleanContent.replace(client.user.username, '').replace('@', '').toLowerCase().replace(' ', '').trim();
-			console.log('\'' + message.cleanContent + '\' remove \'' + (client.user.username) + '\' = \'' + resultMessage + '\'' + ' ' + (resultMessage == 'merci'));
-			if (resultMessage == 'merci') {
+			console.log('\'' + message.cleanContent + '\' remove \'' + (client.user.username) + '\' = \'' + resultMessage + '\'' + ' ' + (resultMessage.toLowerCase() == 'merci'.toLowerCase()));
+			if (resultMessage.toLowerCase() == 'merci'.toLowerCase()) {
 				return message.reply('de rien :Finger_Guns:');
 			}
 			return message.channel.send(':robot: *Fired up and ready to serve.*');
