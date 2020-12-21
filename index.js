bee002393508a17bdec1afede444ce8badebe777
Vikanya <flavien.birette@orange.fr>
2020-12-21 02:22:03 +0100

    ca marche

--- index.js
+++ index.js
@@ -130,6 +130,8 @@ client.on('message', message => {
 		/* message.channel.send(message.mentions.users.first() + ' and ' + client.user
 			+ ' test ' + (message.mentions.users.first().equals(client.user)));*/
 		if (message.mentions.users.first().equals(client.user)) {
+			const resultMessage = message.cleanContent.replace(client.user.username, '').trim().toLowerCase();
+			if (resultMessage === 'merci') return message.reply('de rien :Finger_Guns:');
 			return message.channel.send(':robot: *Fired up and ready to serve.*');
 		}
 	}
