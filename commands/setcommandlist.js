8a4ad2cab568e8b1e15730e8731d38abc190e22e
Vikanya <flavien.birette@orange.fr>
2021-01-04 18:55:47 +0100

    erqgesrqgq<z

--- commands/setcommandlist.js
+++ commands/setcommandlist.js
@@ -2,6 +2,14 @@ module.exports = {
 	name: 'setcommandlist',
 	description: 'Lists all commands added with the !setcommand command.',
 	execute(message, args, keyv) {
-		message.reply('En fait c\'est pas possible avec le système que j\'utilise.')
+		try
+		{
+			console.log(keyv.opts.store.query('SELECT * FROM keyv;'));
+		}
+		catch (error) 
+		{
+			console.error(error);
+			message.reply('En fait c\'est pas possible avec le système que j\'utilise.');
+		}
 	},
 };
