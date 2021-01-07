092a14203a6814d221d51f06f168521641ff10e3
Vikanya <flavien.birette@orange.fr>
2021-01-07 23:02:45 +0100

    insta command

--- commands/ig.js
+++ commands/ig.js
@@ -1,5 +1,4 @@
-const fetch = require("node-fetch")
-const cheerio = require("cheerio")
+const userInstagram = require("user-instagram");
 
 module.exports = {
 	name: 'ig',
@@ -11,32 +10,12 @@ module.exports = {
 		
 		console.log('try fetch ' + args);
 
-		fetch(args).then(res => res.text())
-		.then(html => {
+		const post = userInstagram.getPostData('CJvyagtFR_k')
+		  .then(console.log)
+		  .catch(console.error)
 
-			console.log(html);
-		    const $ = cheerio.load(html)
-		    const title = $("a[class='sqdOP yWX7d     _8A5w5   ZIAjV ']")[0]
-		    const thumbnail = $("img[data-testid='user-avatar']")[0]
 
-		    console.log(title ? title : "no title")
-		    console.log(thumbnail ? thumbnail : "no thumbnail")
 
-		    const instaEmbed = new Discord.MessageEmbed()
-				.setColor('#000000')
-				.setTitle(title)
-				.setURL(args)
-				.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-				.setDescription('Some description here')
-				.setThumbnail(thumbnail.src)
-				.addFields(
-					{ name: 'Regular field title', value: 'Some value here' },
-					{ name: '\u200B', value: '\u200B' },
-					{ name: 'Inline field title', value: 'Some value here', inline: true },
-					{ name: 'Inline field title', value: 'Some value here', inline: true },
-				);
-
-			message.channel.send(instaEmbed);
-		});
+		console.log('result ' + post);
 	},
 };
