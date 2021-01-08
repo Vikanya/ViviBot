5b5b8eb2e40f7e439701e3909268840a5a08415a
Vikanya <flavien.birette@orange.fr>
2021-01-09 00:01:45 +0100

    zrgz

--- commands/ig.js
+++ commands/ig.js
@@ -94,7 +94,7 @@ module.exports = {
 
 					
 					console.log('create collector');
-					let collector = await message.createReactionCollector(filter, { time: 100000 });
+					let collector = await newMessage.createReactionCollector(filter, { time: 100000 });
 					collector.on('collect', (reaction, user) => 
 					{
 						console.log('reaction ');
