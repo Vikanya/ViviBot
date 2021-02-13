f88263a55e12a271822ba2352b9008be8512a850
Vikanya <flavien.birette@orange.fr>
2021-02-13 02:27:03 +0100

    qsc

--- commands/setcommandlist.js
+++ commands/setcommandlist.js
@@ -7,15 +7,9 @@ module.exports = {
 		try
 		{
 			var listMess = '';
-			redisScan({
-		        redis: redis,
-		        each_callback: function (type, key, subkey, value, cb) {
-		            console.log(type, key, subkey, value);
-		            listMess += value + ', ';
-		        }
-		    });
+			redis.keys('*').then(res => console.log("res " + res));
 
-			message.reply(listMess);
+			//message.reply(listMess);
 			/*redis.opts.store.query('SELECT * FROM redis;').then(keys => {
 				let finalString = '';
 				keys.forEach(element => finalString += element.key.toString().replace('redis:', '') + ', ');
