dea39fc2898ee67374d761fc7998df4f825deac3
Vikanya <flavien.birette@orange.fr>
2021-02-13 01:52:40 +0100

    dfdg

--- commands/setcommandlist.js
+++ commands/setcommandlist.js
@@ -6,12 +6,16 @@ module.exports = {
 	execute(message, args, redis) {
 		try
 		{
+			var listMess = '';
 			redisScan({
 		        redis: redis,
 		        each_callback: function (type, key, subkey, value, cb) {
 		            console.log(type, key, subkey, value);
+		            listMess += value + ', ';
 		        }
 		    });
+
+			message.reply(listMess);
 			/*redis.opts.store.query('SELECT * FROM redis;').then(keys => {
 				let finalString = '';
 				keys.forEach(element => finalString += element.key.toString().replace('redis:', '') + ', ');
