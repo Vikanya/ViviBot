d04ec0d5a4ba164db710067d0671dc6c2110f4c7
Vikanya <flavien.birette@orange.fr>
2021-09-27 00:55:29 +0200

    aqzsef

--- commands/curse.js
+++ commands/curse.js
@@ -9,6 +9,11 @@ module.exports = {
 		const data = [];
 		const { commands } = message.client;
 
+		let level = 2;
+
+		if (args[0][0] == "_") {
+			level = args.shift().replace("_", "");
+		}
 
 		let remainingArgs = '';
 		args.forEach(element => remainingArgs += element + ' ');
@@ -19,7 +24,15 @@ module.exports = {
 		for (let i = remainingArgs.length; i > 0; i--) 
 		{
 			console.log("before " + remainingArgs);
- 			remainingArgs = remainingArgs.slice(0, i) + String.fromCharCode((Math.ceil(Math.random() * (879 - 768) + 768))/*.toString(16)*/) + remainingArgs.slice(i);
+			let text = remainingArgs.slice(0, i);
+
+			for (let j = 0; j < level; j++)
+			{
+ 				text += String.fromCharCode(Math.ceil(Math.random() * (879 - 768) + 768));
+			} 
+ 			text += remainingArgs.slice(i);
+
+ 			//remainingArgs = remainingArgs.slice(0, i) + String.fromCharCode(Math.ceil(Math.random() * (879 - 768) + 768)) + remainingArgs.slice(i);
 			console.log("after  " + remainingArgs);
 		}
 
